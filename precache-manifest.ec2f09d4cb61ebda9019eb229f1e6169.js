b6283aa8f6ae13d0a0fdd360534c808a7f6e09eb
Code Sandbox <hello@codesandbox.io>
2021-12-13 01:04:44 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ec2f09d4cb61ebda9019eb229f1e6169.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61ea37fcddaced0ccd5e4d92b3b7243",
+    "url": "https://hamosapiens.github.io/csb-ujn16/index.html"
+  },
+  {
+    "revision": "8ba1c06c9fc18e1e3ac8",
+    "url": "https://hamosapiens.github.io/csb-ujn16/static/css/main.801207de.chunk.css"
+  },
+  {
+    "revision": "d6921796ba79e9313b27",
+    "url": "https://hamosapiens.github.io/csb-ujn16/static/js/2.47660161.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "https://hamosapiens.github.io/csb-ujn16/static/js/2.47660161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "176723e74205e1d78f62",
+    "url": "https://hamosapiens.github.io/csb-ujn16/static/js/3.0ac68d98.chunk.js"
+  },
+  {
+    "revision": "8ba1c06c9fc18e1e3ac8",
+    "url": "https://hamosapiens.github.io/csb-ujn16/static/js/main.5bcfed58.chunk.js"
+  },
+  {
+    "revision": "158ef0824451bd006884",
+    "url": "https://hamosapiens.github.io/csb-ujn16/static/js/runtime-main.35b29fd0.js"
+  }
+]);
